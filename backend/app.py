f1dd9ffab6385c00db775c8548a77280ab63ee3e
Efrain Palencia <epalen6@wgu.edu>
2025-01-23 09:09:23 -0500

    Defines models and configures CRUD operations

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from routes import api_routes
+from models import init_db
 
 app = Flask(__name__)
 CORS(app)
+init_db(app)
 
 # Load Routes
 app.register_blueprint(api_routes, url_prefix="/api")
